2f8bfc75efa4dc80024c1fa359c8c2c23d63666b
Lashaun Corinna <lcorinna@mi-u3.msk.21-school.ru>
2021-11-26 20:33:57 +0300

    day three

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lcorinna <lcorinna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 19:24:31 by lcorinna          #+#    #+#             */
-/*   Updated: 2021/11/24 20:01:32 by lcorinna         ###   ########.fr       */
+/*   Updated: 2021/11/26 20:31:52 by lcorinna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,19 +49,27 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (newstr);
 }
 
-void	*ft_memset(void *b, int c, size_t len)
+char	*ft_substr_free(char const *s, unsigned int start, size_t len)
 {
+	char	*newstr;
 	int		i;
-	char	*e;
+	int		count;
 
-	e = b;
 	i = 0;
-	while (i < (int) len)
-	{
-		e[i] = c;
-		i++;
-	}
-	return (e);
+	count = ft_strlen(s) - start;
+	if (count > (int) len)
+		count = (int) len;
+	if (count <= 0)
+		count = 0;
+	newstr = (char *) malloc(sizeof(char) * (count + 1));
+	if (newstr == NULL)
+		return (newstr);
+	while (s[start] != 0 && len-- != 0 && count != 0)
+		newstr[i++] = s[start++];
+	newstr[i] = '\0';
+	free((char *) s);
+	s = NULL;
+	return (newstr);
 }
 
 char	*ft_strchr(const char *s, int c)
@@ -82,7 +90,7 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin_free(char const *s1, char const *s2)
 {
 	int		i;
 	int		p;
@@ -99,31 +107,149 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	while (s2[i])
 		join[p++] = s2[i++];
 	join[p] = '\0';
+	// printf("%p-%s\n", s1, s1);
+	// printf("%p-%s\n", s2, s2);
+	free((char *) s1);
+	s1 = NULL;
+	free((char *) s2);
+	s2 = NULL;
 	return (join);
 }
 
+// char	*get_next_line(int fd)
+// {
+// 	int				j;
+// 	int				i;
+// 	char			*str;
+// 	static char		*reserv;
+// 	str = (char *) malloc(sizeof(char) * (5 + 1)); //BUFFER_SIZE
+// 	i = read(fd, str, 5);
+// 	//printf("%d\n", i);
+// 	if (i == 0)
+// 		return (NULL);
+// 	if (reserv != NULL)
+// 	{
+// 		//read(fd, str, 5);
+// 		reserv = (ft_strjoin(reserv, str));
+// 	}
+// 	if (reserv == NULL)
+// 		reserv = ft_substr(str, 0, 5); //BUFFER_SIZE
+// 	while ((ft_strchr(reserv, '\n')) == NULL)
+// 	{
+// 		i = read(fd, str, 5); //BUFFER_SIZE
+// 		if (i < 1)
+// 			return (reserv);
+// 		reserv = ft_strjoin(reserv, str);
+// 	}
+// 	i = 0;
+// 	j = 0;
+// 	while (reserv[i] != '\n')
+// 	{
+// 		i++;
+// 		if (reserv[i] == '\0')
+// 			return (reserv);
+// 	}
+// 	while (reserv[i + j] != '\0')
+// 		j++;
+// 	str = ft_substr(reserv, 0, i);
+// 	reserv = ft_substr(reserv, (i + 1), j);
+// 	printf("reserv: %s", reserv);
+// 	return (str);
+// }
+
 char	*get_next_line(int fd)
 {
-	char			*new_str;
-	static char		*reserv;
+	int				j;
+	int				i;
 	char			*str;
+	static char		*res;
 
-	str = (char *) malloc(sizeof(char) * (5 + 1));//BUFFER_SIZE)
-	new_str = (char *) malloc(sizeof(char) * 1);
-	ft_memset(str, '\0', 5);//BUFFER_SIZE
-	// if (reserv)//при втором заходе забирать остатки в новую строку
-	// {
-	// 	new_str = reserv;
-	// }
-	// while ((ft_strchr(str, 10)) == NULL)
-	// {
-	read(fd, str, 5);//нужно чтобы работало от 1 до 1000000 //BUFFER_SIZE
-	//str[5] = '\0';
-	new_str = ft_strjoin(new_str, str);
-	//}
-	//printf("%s\n\n", str);
-	//new_str = ft_substr(str, 0, i); // когда-то вернуть NULL
-	return (new_str);
+	if (res != NULL)
+	{
+		if ((ft_strchr(res, '\n') != NULL))
+		{
+			i = 0;
+			j = 0;
+			while (res[i++] != '\n')
+			{
+				if (res[i] == '\0')
+					return (res);
+			}
+			while (res[i + j] != '\0')
+				j++;
+			str = ft_substr(res, 0, i);
+			res = ft_substr(res, i, j);
+			return (str);
+		}
+		while ((ft_strchr(res, '\n')) == NULL)
+		{
+			str = (char *) malloc(sizeof(char) * (5 + 1));
+			i = read(fd, str, 5);
+			str[i] = '\0';
+			if (i <= 0 || str[i - 1] == '\0')
+			{
+				str = ft_strjoin_free(res, str);
+				res = NULL;
+				return (str);
+			}
+			res = ft_strjoin_free(res, str);
+		}
+		i = 0;
+		j = 0;
+		while (res[i++] != '\n')
+		{
+			if (res[i] == '\0')
+				return (res);
+		}
+		while (res[i + j] != '\0')
+			j++;
+		str = ft_substr(res, 0, i);
+		res = ft_substr(res, i, j);
+		return (str);
+	}
+	else if (res == NULL)
+	{
+		res = (char *) malloc(sizeof(char) * (5 + 1));
+		i = read(fd, res, 5);
+		res[i] = '\0';
+		if (i <= 0)
+		{
+			free (res);
+			res = NULL;
+			return (NULL);
+		}
+		res = ft_substr_free(res, 0, i);
+		if (i < 5)
+			return (res);
+		while ((ft_strchr(res, '\n')) == NULL)
+		{
+			str = (char *) malloc(sizeof(char) * (5 + 1));
+			i = read(fd, str, 5);
+			if (i <= 0 || str[i - 1] == '\0')
+			{
+				free(str);
+				return (res);
+			}
+			res = ft_strjoin_free(res, str);
+		}
+	}
+	i = 0;
+	j = 0;
+	while (res[i++] != '\n')
+	{
+		if (res[i] == '\0')
+		{
+			str = ft_substr(res, 0, ft_strlen(res));
+			free(res);
+			res = NULL;
+			return (res);
+		}
+	}
+	while (res[i + j] != '\0')
+		j++;
+	str = ft_substr(res, 0, i);
+	res = ft_substr(res, i, j);
+	return (str);
 }
 
 int	main(void)
@@ -132,13 +258,23 @@ int	main(void)
 	char	*gnl;
 
 	fd = open("text.txt", O_RDONLY);
-	// "open" return "fd", this allow you to call the file
-	gnl = get_next_line(fd);
+	while ((gnl = get_next_line(fd)) != NULL)
+	{
+		printf("%s", gnl);
+	}
+	// gnl = get_next_line(fd);
+	// printf("%s\n", gnl);
+	// free(gnl);
+	// gnl = get_next_line(fd);
+	// printf("%s\n", gnl);
+	// free(gnl);
+	// gnl = get_next_line(fd);
+	// printf("%s\n", gnl);
+	// free(gnl);
+	// gnl = get_next_line(fd);
 	printf("%s\n", gnl);
-	// while ((gnl = get_next_line(fd)) != NULL)
-	// {
-	// 	printf("%s\n", gnl);
-	// }
+	//free(gnl);
+	close (fd);
 	return (0);
 }
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lcorinna <lcorinna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 19:26:30 by lcorinna          #+#    #+#             */
-/*   Updated: 2021/11/23 19:30:59 by lcorinna         ###   ########.fr       */
+/*   Updated: 2021/11/25 18:44:53 by lcorinna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
-// # include <stddef.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
 // including the necessary libraries for all functions
-// инклюдим необходимые библиотеки для всех функций
 
 char	*get_next_line(int fd);
 // we write the name of the functions that we use in the project
-// пишем название функций которые успользуем в проекте
 
 #endif
